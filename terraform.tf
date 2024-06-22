5d4f28bad80588a8e7b0566eb837daaecbcd08dc
Abhijithsaralay <66477665+Abhijithsaralay@users.noreply.github.com>
2024-06-22 16:05:39 +0530

    Update terraform.tf with module added and region buddy1

--- terraform.tf
+++ terraform.tf
@@ -9,3 +9,18 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+module "aws-s3-bucket" {
+	source  = "cmrit939.scalr.io/acc-v0od9bfiu3g87ptbs/aws-s3-bucket/test"
+	version = "5.0.0"
+
+	# Set 1 required variable below.
+
+	# (Optional, Forces new resource) The name of the bucket. If omitted, Terraform will assign a random, unique name.
+ 	bucket = <value>
+}
